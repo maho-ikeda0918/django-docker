c5abe8d62bb250e3c1521765d9c771f21f47e614
ak-yoshimatsu <codetrain.akihiro.yoshimatsu@gmail.com>
2024-02-22 23:27:54 +0900

    マイグレーションファイル作成

--- src/info/models.py
+++ src/info/models.py
@@ -1,3 +1,15 @@
+import uuid
+
 from django.db import models
 
+
 # Create your models here.
+class Info(models.Model):
+    class Meta:
+        db_table = "information"
+
+    uuid = models.UUIDField(default=uuid.uuid4, unique=True)
+    title = models.CharField(max_length=32)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
